a424b7b7453377b0d839428e2535cf41d51590fc
abdalhababsah <abdalhababseh@outlook.com>
2024-11-19 12:19:08 +0300

    header modification responsive

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -66,7 +66,7 @@
                     <img src="{{ asset('assets/tiptop.png') }}" alt="Logo" />
                 </a>
             </div>
-    
+
         </div>
 
     </nav>
@@ -109,6 +109,10 @@
     /* Desktop Dropdown */
     .desktop-nav .dropdown {
         position: relative;
+    }
+
+    .desktop-nav li {
+        width: 103px;
     }
 
     .desktop-nav .dropdown-menu {
